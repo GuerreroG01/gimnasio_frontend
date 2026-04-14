749c3fb2dfd9f8e3bbd601e92294d51b369a649b
Gabriel Guerrero <guerrerog675@gmail.com>
2026-04-14 12:32:44 -0600

    Modificación en barchart de ingresos

--- src/Components/Home/GananciasMensualesChart.js
+++ src/Components/Home/GananciasMensualesChart.js
@@ -1,8 +1,17 @@
 import React, { useEffect } from "react";
-import { BarChart, useAnimateBar, useDrawingArea } from "@mui/x-charts";
+import { BarChart } from "@mui/x-charts";
 import { useTheme } from "@mui/material/styles";
-import { Box, FormControl, InputLabel, MenuItem, Select, Typography, Snackbar, Alert,
-    useMediaQuery } from "@mui/material";
+import {
+    Box,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+    Typography,
+    Snackbar,
+    Alert,
+    useMediaQuery,
+} from "@mui/material";
 import PagoService from "../../Services/PagoService";
 
 const mesesNombres = [
@@ -34,11 +43,6 @@ const GananciasMensualesChart = ({ pagosData }) => {
         const fetchMeses = async () => {
             try {
                 const response = await PagoService.getMesesConPagos(añoSeleccionado);
-                if (!response || response.length === 0) {
-                    setChartData(null);
-                    setOpenSnackbar(true);
-                    return;
-                }
 
                 const formattedData = response.map(item => ({
                     mes: mesesNombres[item.mes - 1],
@@ -61,6 +65,7 @@ const GananciasMensualesChart = ({ pagosData }) => {
         <Box sx={{ width: "100%", textAlign: "center" }}>
             <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", mb: 2 }}>
                 <Typography variant="h6">Ganancias Mensuales</Typography>
+
                 <FormControl variant="filled" sx={{ minWidth: 120 }}>
                     <InputLabel>Año</InputLabel>
                     <Select
@@ -81,15 +86,31 @@ const GananciasMensualesChart = ({ pagosData }) => {
                     dataset={chartData}
                     layout="horizontal"
                     xAxis={[
-                        { id: "nio", min: 0, valueFormatter: v => `C$${v.toLocaleString()}`, disableTicks: true, tickLabelStyle: { display: "none" }},
-                        { id: "usd", min: 0, valueFormatter: v => `$${v.toFixed(2)}` }
+                        {
+                            id: "nio",
+                            valueFormatter: (v) => `C$${v.toLocaleString()}`,
+                        }
+                    ]}
+                    yAxis={[
+                        { scaleType: "band", dataKey: "mes", width: 110 }
                     ]}
                     series={[
-                        { id: "gananciasNIO", dataKey: "gananciasNIO", label: "Ganancias NIO", xAxisId: "nio", color: "#1976d2", valueFormatter: (v) => `C$${v.toLocaleString()}`, barLabel: (v) => `C$${v.value.toLocaleString()}`},
-                        { id: "gananciasUSD", dataKey: "gananciasUSD", label: "Ganancias USD", xAxisId: "usd", color: "#2e7d32", valueFormatter: (v) => `$${v.toFixed(2)}`, barLabel: (v) => `$${v.value.toLocaleString()}`},
+                        {
+                            id: "ganancias",
+                            dataKey: "gananciasNIO",
+                            label: "Ingresos",
+                            color: "#19d241",
+
+                            valueFormatter: (value, context) => {
+                                const dataIndex = context?.dataIndex;
+                                const row = chartData[dataIndex];
+
+                                if (!row) return "";
+
+                                return `C$${row.gananciasNIO.toLocaleString()} | $${row.gananciasUSD.toFixed(2)}`;
+                            },
+                        }
                     ]}
-                    yAxis={[{ scaleType: "band", dataKey: "mes", width: 110 }]}
-                    slots={{ bar: BarShadedBackground }}
                 />
             ) : (
                 <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: chartHeight }}>
@@ -104,24 +125,12 @@ const GananciasMensualesChart = ({ pagosData }) => {
                 autoHideDuration={3000}
                 onClose={() => setOpenSnackbar(false)}
             >
-                <Alert severity="error">No se encontraron datos para el año seleccionado.</Alert>
+                <Alert severity="error">
+                    No se encontraron datos para el año seleccionado.
+                </Alert>
             </Snackbar>
         </Box>
     );
 };
 
 export default GananciasMensualesChart;
-
-function BarShadedBackground(props) {
-    const { ownerState, ...other } = props;
-    const theme = useTheme();
-    const animatedProps = useAnimateBar(props);
-    const { width } = useDrawingArea();
-
-    return (
-        <>
-            <rect {...other} fill={(theme.vars || theme).palette.text.primary} opacity={theme.palette.mode === "dark" ? 0.05 : 0.08} x={other.x} width={width} />
-            <rect {...other} opacity={ownerState?.isFaded ? 0.3 : 1} {...animatedProps} />
-        </>
-    );
-}
