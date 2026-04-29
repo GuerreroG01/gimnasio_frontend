f188ce3bb957c67c19c3cde940e78344fded670f
Gabriel Guerrero <guerrerog675@gmail.com>
2026-04-29 12:39:31 -0600

    Corrección del módulo de progresos

--- src/Components/Progresos/ClienteProgreso.js
+++ src/Components/Progresos/ClienteProgreso.js
@@ -125,7 +125,7 @@ export default function ClienteProgreso({ theme, clienteIdInput, setClienteIdInp
                                 <InputAdornment position="end">
                                 <Tooltip title="Buscar Progreso">
                                     <IconButton
-                                    onClick={handleFetchProgresos}
+                                    onClick={() => handleFetchProgresos()}
                                     edge="end"
                                     color="primary"
                                     >
